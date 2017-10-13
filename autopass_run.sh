dcb40604d1798f29adf15ea25b41d0872f28ab2e
dave flynn <david.flynn@microfocus.com>
2017-10-13 11:57:56 -0700

    updated the autopass tag

--- autopass_run.sh
+++ autopass_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name autopass --hostname autopass.aos.com --net demo-net -p 5814:5814 --ip=172.50.10.10 --restart=always -d admpresales/autopass
+docker run --name autopass --hostname autopass.aos.com --net demo-net -p 5814:5814 --ip=172.50.10.10 --restart=always -d admpresales/autopass:9.3_v2
