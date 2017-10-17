d99c84b98fb275cec5359141afa2ad8e6468dfc6
dave flynn <david.flynn@microfocus.com>
2017-10-17 16:09:30 -0700

    switched to run in background

--- lft_agent_run.sh
+++ lft_agent_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run --rm --name leanft -p 5095:5095 -p 5900:5900 \
+docker run --rm -d --name leanft -p 5095:5095 -p 5900:5900 \
 -e LFT_LIC_SERVER=localhost -e LFT_LIC_ID=23078 -e VERBOSE=true \
 --net "host" functionaltesting/leanft-chrome:14.01
 
