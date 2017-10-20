b4a4583926632e9551c8d2436e316287e90980f8
Dave Flynn <david.flynn@hpe.com>
2017-10-20 10:03:42 -0500

    Delete put_Octane_Gherkin_in_GitRepo.sh

--- populate_git_scripts/put_Octane_Gherkin_in_GitRepo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git clone --mirror https://github.houston.softwaregrp.net/AMSPreSales-Demos/Octane_Gherkin.git
-
-docker exec -it devops rm -rf /GitRepo/Octane_Gherkin.git
-
-docker cp Octane_Gherkin.git devops:/GitRepo
-
-rm -rf ./Octane_Gherkin.git
