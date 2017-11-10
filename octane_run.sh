374f58cc814c7a54c5195581af7816b2392f5916
dave flynn <david.flynn@microfocus.com>
2017-11-09 21:27:14 -0800

    change tag number

--- octane_run.sh
+++ octane_run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run -d -p 1099:1099 -p 8085:8080 -p 9081:9081 -p 9082:9082 --name octane --hostname octane.aos.com --net demo-net -e OCTANE_HOST=nimbusserver.aos.com --shm-size=2g admpresales/octane:12.55.6.67_dis
+docker run -d -p 1099:1099 -p 8085:8080 -p 9081:9081 -p 9082:9082 \
+--name octane --hostname octane.aos.com --net demo-net -e OCTANE_HOST=nimbusserver.aos.com --shm-size=2g \
+admpresales/octane:12.55.7.84_dis
