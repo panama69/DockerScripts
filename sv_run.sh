1251ecbb3f26b5938ce25d52527b7397379b36a3
dave flynn <david.flynn@microfocus.com>
2017-12-20 12:31:10 -0800

    modified to match Nimbus 2.0.  future will mod to accept commandline arg

--- sv_run.sh
+++ sv_run.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
-SERVER_IP=$(grep -i 'DockerServer' /etc/hosts | awk '{print $1}' | sort --unique)
+SV_TAG="4.10-beta1"
+SV_DB_TAG="xe11g"
 
-docker run -d --name sv-db --network demo-net admpresales/sv-db:latest
+echo "Using DB: $SV_DB_TAG"
+echo "Using SV: $SV_TAG"
 
-docker run -d --name sv-server -e SV_SERVER_AGENT_PORTS=7000-7019 -e SV_USER=demo -e SV_PASSWORD=Password1 -e SV_GROUPS="SVM Users,SV Server Administrators" --network demo-net -p 6085:6085 -p 7000-7019:7000-7019 -h $SERVER_IP -v /etc/hpe-sv-server/ -v /var/log/hpe-sv-server/ -v /var/lib/hpe-sv-server/ -v /var/cache/hpe-sv-server/ admpresales/sv-server
+docker run -d --name sv-db --network demo-net admpresales/sv-db:$SV_DB_TAG
 
-docker run -d --name sv-svm --network demo-net -p 6086:6086 -h $SERVER_IP --volumes-from sv-server admpresales/sv-svm
+docker run -d --name sv-server \
+    -e SV_SERVER_AGENT_PORTS=7000-7019 \
+    -e SV_USER=demo \
+    -e SV_PASSWORD=Password1 \
+    -e SV_GROUPS="SVM Users,SV Server Administrators" \
+    --network demo-net \
+    -p 6085:6085 \
+    -p 7000-7019:7000-7019 \
+    -h NimbusServer \
+    -v /etc/hpe-sv-server/ \
+    -v /var/log/hpe-sv-server/ \
+    -v /var/lib/hpe-sv-server/ \
+    -v /var/cache/hpe-sv-server/ \
+    admpresales/sv-server:$SV_TAG
+
+docker run -d --name sv-svm \
+    --network demo-net \
+    -p 6086:6086 \
+    -h NimbusServer \
+    --volumes-from sv-server \
+    admpresales/sv-svm:$SV_TAG
