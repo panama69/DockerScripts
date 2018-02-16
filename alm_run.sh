f74d8de7c19b10b2de3de2b22858ad58d14c8809
dave flynn <david.flynn@microfocus.com>
2018-02-16 12:48:06 -0800

    put on diff lines for easier reading

--- alm_run.sh
+++ alm_run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker run -d -p 8082:8080 -p 1521:1521 --name alm --hostname alm.aos.com --net demo-net --shm-size=2g admpresales/alm:12.55_di
+docker run -d \
+-p 8082:8080 -p 1521:1521 \
+--name alm --hostname alm.aos.com --net demo-net --shm-size=2g \
+admpresales/alm:12.55_di
