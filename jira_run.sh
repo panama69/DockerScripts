2760ec7cc6ed8341dd9f2beb5b4299ba9059f1c7
panama69 <github@flynnshome.com>
2018-03-05 13:55:34 -0800

    init

--- jira_run.sh
+++ jira_run.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-#-v /home/demo/atlassian/jira:/var/atlassian/jira -v /home/demo/atlassian/logs:/opt/atlassian/jira/logs \
-#-v /home/demo/atlassian/jira:/var/atlassian/jira \
-#-v /var/atlassian/jira:/var/atlassian/jira -v /opt/atlassian/jira \
-#docker run --name jira --hostname jira.aos.com --net demo-net -d -p 8080:8080 -v /home/demo/jira:/var/Atlassian/jira -v /home/demo/jiralogs:/opt/Atlassian/jira/logs cptactionhank/Atlassian-jira-software:7.53
+docker run --name jira --hostname jira.aos.com \
+   --net demo-net --detach --publish 8080:8080 \
+   cptactionhank/atlassian-jira-software:7.6.3
 
-docker run --name jira --hostname jira.aos.com --net demo-net -d -p 8080:8080 \
--v /home/demo/jira:/var/Atlassian/jira -v /home/demo/jiralogs:/opt/Atlassian/jira/logs \
-cptactionhank/atlassian-jira-software:7.5.3
