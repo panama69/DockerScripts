2fcf0d1af9c54dcd9459f259d929ffe672cfe93d
panama69 <github@flynnshome.com>
2018-03-24 10:27:50 -0700

    run changes

--- jira_run.sh
+++ jira_run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+#   cptactionhank/atlassian-jira-software:7.6.3
 docker run --name jira --hostname jira.aos.com \
-   --net demo-net --detach --publish 8080:8080 \
-   cptactionhank/atlassian-jira-software:7.6.3
+   --net demo-net --detach --publish 9090:8080 \
+   cptactionhank/atlassian-jira-software:7.8.0
 
