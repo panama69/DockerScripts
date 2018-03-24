36f6ba3e330767e80484f4142e29a2c6f93a319b
panama69 <github@flynnshome.com>
2018-03-24 10:27:31 -0700

    run changes

--- devops_run.sh
+++ devops_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -d -p 8090:8080 -p 50000:50000 -p 9022:22 \
---name devops --hostname devops.aos.com --net demo-net admpresales/devops:1.1.2.2
+docker run -d -p 8090:8080 -p 8091:80 -p 50000:50000 -p 9022:22 \
+--name devops --hostname devops.aos.com --net demo-net admpresales/devops:1.1.3.2
