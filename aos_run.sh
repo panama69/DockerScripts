dddaebdc47364903b5b827d46469fb2e54c5a1f8
jeff-kingston <37638400+jeff-kingston@users.noreply.github.com>
2018-06-11 13:29:10 -0700

    Update main IP domain
    update to nimbusserver.aos.com for SRF tunnel

--- aos_run.sh
+++ aos_run.sh
@@ -9,4 +9,4 @@ docker run -d -p 5432:5432 --name aos_postgres --hostname aosdb.aos.com --net de
 
 docker run -p 8001:8080 --name aos_accountservice --hostname aosaccount.aos.com -e "POSTGRES_PORT=5432" -e "POSTGRES_IP=aos_postgres" -e "MAIN_PORT=8000" -e "ACCOUNT_PORT=8001" -e 'MAIN_IP=nimbusserver.aos.com' -e "ACCOUNT_IP=nimbusserver" -e "PGPASSWORD=admin" --net demo-net -d ${IMAGE_ACCTSERVICE}
 
-docker run -p 8000:8080 --name aos_main --hostname aosweb.aos.com -e "POSTGRES_PORT=5432" -e "POSTGRES_IP=aos_postgres" -e "MAIN_PORT=8000" -e "ACCOUNT_PORT=8001" -e 'MAIN_IP=nimbusserver' -e "ACCOUNT_IP=nimbusserver.aos.com" -e "PGPASSWORD=admin" --net demo-net -d ${IMAGE_MAIN}
+docker run -p 8000:8080 --name aos_main --hostname aosweb.aos.com -e "POSTGRES_PORT=5432" -e "POSTGRES_IP=aos_postgres" -e "MAIN_PORT=8000" -e "ACCOUNT_PORT=8001" -e 'MAIN_IP=nimbusserver.aos.com' -e "ACCOUNT_IP=nimbusserver.aos.com" -e "PGPASSWORD=admin" --net demo-net -d ${IMAGE_MAIN}
