2aab42b141a643435b874bd66bc526c9f5cf909a
dave flynn <david.flynn@hpe.com>
2018-07-16 15:00:31 -0700

    updated for new format

--- android_emulator_run.sh
+++ android_emulator_run.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
- 
-docker run -d --rm -e "DEVICE=Nexus7-5.1.1" -e GPU="off" -e "CONSOLE_PORT=5554" -p "5554:5554" -p "5555:5555" -p 6080:6080 -p 8080:8080 --net demo-net --name nexus5 --privileged --entrypoint=tail admpresales/android-emulator -f /dev/null
+
+if [[ $# -gt 0 ]]
+then
+   IMAGE=$1
+else
+   IMAGE=`grep "android-emulator " env|awk '{print $2}'`
+fi
+echo -e "Starting image: \n\t${IMAGE}"
+
+docker run -d --rm \
+   -e "DEVICE=Nexus7-5.1.1" -e GPU="off" -e "CONSOLE_PORT=5554" \
+   -p "5554:5554" -p "5555:5555" -p 6080:6080 -p 8080:8080 \
+   --name nexus5 --net demo-net --privileged ${IMAGE}
