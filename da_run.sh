afab020cbefe50838e4390955cbbf11b7dd459a2
dave flynn <david.flynn@hpe.com>
2018-07-25 13:42:35 -0700

    added echo

--- da_run.sh
+++ da_run.sh
@@ -14,5 +14,6 @@ else
    fi
 fi
 echo -e "Starting image: \n\t${IMAGE}"
+echo "open in a browser http://nimbusserver.aos.com:8089/da"
 
 docker run -d -p 8089:8080 -p 7918:7918 --name da --net demo-net ${IMAGE}
